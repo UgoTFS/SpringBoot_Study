513e02923d33013a01e5d92347f4b433cf358b38
Ugo <ugo.tomio@gmail.com>
2023-05-03 15:01:41 -0300

    commits finais para a api de estudo sobre springboot

---
+++ api/src/test/java/med/voll/api/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,10 @@
+package med.voll.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class AutenticacaoControllerTest {
+  @Test
+  void testEfetuarLogin() {
+
+  }
+}
---
+++ api/src/test/java/med/voll/api/controller/PacienteControllerTest.java
@@ -0,0 +1,30 @@
+package med.voll.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class PacienteControllerTest {
+  @Test
+  void testAtualizar() {
+
+  }
+
+  @Test
+  void testCadastrar() {
+
+  }
+
+  @Test
+  void testDeletar() {
+
+  }
+
+  @Test
+  void testDetalhar() {
+
+  }
+
+  @Test
+  void testListar() {
+
+  }
+}
